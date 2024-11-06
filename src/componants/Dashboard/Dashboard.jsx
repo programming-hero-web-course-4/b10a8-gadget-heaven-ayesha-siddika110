163c99d90d28f1676548940e05c850881b512a4d
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-11-06 20:36:38 +0600

    purchase area done

--- src/componants/Dashboard/Dashboard.jsx
+++ src/componants/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import { getStorageToCartList, getStorageToWishList, removecartData, removeWishL
 import Cart from './Cart';
 import Content from '../Home/content';
 import CartWish from './cartWish';
+import grpImg from '../../assets/Group.png'
 
 
 const Dashboard = () => {
@@ -17,6 +18,19 @@ const Dashboard = () => {
         const storedcartList = getStorageToCartList()
         setcartList(storedcartList)
     }, [data])
+    const [cartlistp, setcartListp] = useState(0)
+    useEffect(() => {
+        const totalPrice = cartlist.reduce((sum, item) => sum + item.price, 0);
+        setcartListp(totalPrice)
+
+    }, [cartlist])
+    // const [sort, setSort] = useState(data)
+
+    // const handleSort = () => {
+    //     const sortData = [...data].sort((a, b) => a.price - b.price)
+    //     setSort(sortData)
+    // }
+
 
     const [wishlist, setwishList] = useState([])
     useEffect(() => {
@@ -38,6 +52,13 @@ const Dashboard = () => {
 
     }
 
+    const handlePurchase = () => {
+        // alert('data removed')
+        // eslint-disable-next-line no-undef
+        my_modal_1.showModal()
+        localStorage.clear()
+    }
+
 
 
 
@@ -51,7 +72,7 @@ const Dashboard = () => {
                     <div className='flex justify-center space-x-4'>
 
                         <Tab><Link className={`border-2 py-2 text-white rounded-3xl px-4 hover:bg-white hover:text-black`}>Read List</Link></Tab>
-                        <Tab><Link className={`border-2 py-2 text-white rounded-3xl px-4 hover:bg-white hover:text-black`} to='/wish'>Wish List</Link></Tab>
+                        <Tab><Link className={`border-2 py-2 text-white rounded-3xl px-4 hover:bg-white hover:text-black`}>Wish List</Link></Tab>
                     </div>
                 </TabList>
 
@@ -62,9 +83,9 @@ const Dashboard = () => {
                             <div className='flex justify-between'>
                                 <h1 className=' text-2xl'>Carts</h1>
                                 <div className='space-x-4 flex items-center'>
-                                    <p className='text-xl font-semibold'>Total Price : </p>
-                                    <Link className='py-2 px-3 rounded-3xl border-[#9538E2] border text-[#9538E2]'>Sort by Price</Link>
-                                    <Link className='py-2 px-3 rounded-3xl border-[#9538E2] border text-[#9538E2]'>Purchase</Link>
+                                    <p className='text-xl font-semibold'>Total Price : {cartlistp}</p>
+                                    <NavLink className='py-2 px-3 rounded-3xl border-[#9538E2] border text-[#9538E2]' >Sort by Price</NavLink>
+                                    <NavLink className='py-2 px-3 rounded-3xl border-[#9538E2] border text-[#9538E2]' onClick={handlePurchase}>Purchase</NavLink>
                                 </div>
                             </div>
 
@@ -77,8 +98,8 @@ const Dashboard = () => {
                     </div>
                 </TabPanel>
                 <TabPanel>
-                    {/* <h2>{wishlist.length}</h2> */} heart
-                    <div className='mt-8 grid grid-cols-3 gap-8'>
+
+                    <div className='mt-8 grid grid-cols-1 gap-8'>
                         {
                             wishlist.map(cart => <CartWish handleWishListRemovedata={handleWishListRemovedata} cart={cart}></CartWish>)
                         }
@@ -87,6 +108,21 @@ const Dashboard = () => {
 
                 </TabPanel>
             </Tabs>
+            
+            <dialog id="my_modal_1" className="modal rounded-2xl">
+                <div className="modal-box flex flex-col justify-center items-center space-y-4 w-[450px] py-6 rounded-xl">
+                    <img className='m-auto' src={grpImg} alt="" />
+                    <h3 className="font-bold text-xl">Payment Successfull</h3>
+                    <p className=" text-gray-500">thanks for purchasing</p>
+                    <p className=" text-gray-500">total price : {cartlistp}</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className=" bg-[#9538E2] text-white py-2 px-5 rounded-3xl text">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
